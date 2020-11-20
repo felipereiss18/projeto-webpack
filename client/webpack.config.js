a27261c2485777ce8744f719bbee1af6b8188e8b
Felipe Reis Silva <felipereiss@gmail.com>
2020-11-20 17:21:15 -0300

    Finalização do projeto

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,12 +2,40 @@ const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webPack = require('webpack');
 
 let plugins = [];
 
 plugins.push(new extractTextPlugin('styles.css'));
 
+plugins.push(new htmlWebpackPlugin({
+    hash: true,
+    minify: {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true
+    },
+    filename: 'index.html',
+    template: __dirname + '/main.html'
+}));
+
+plugins.push(new webPack.ProvidePlugin({
+    '$': 'jquery/dist/jquery.js',
+    'jQuery': 'jquery/dist/jquery.js'
+}))
+
+plugins.push(new webPack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.bundle.js'
+}));
+
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
+
 if(process.env.NODE_ENV === 'production'){
+    SERVICE_URL = JSON.stringify('http://endereco_producao')
+
+    plugins.push(new webPack.optimize.ModuleConcatenationPlugin());
     plugins.push(new babiliPlugin());
 
     plugins.push(new optimizeCSSAssetsPlugin({
@@ -21,12 +49,16 @@ if(process.env.NODE_ENV === 'production'){
     }))
 }
 
+plugins.push(new webPack.DefinePlugin({SERVICE_URL}))
+
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
